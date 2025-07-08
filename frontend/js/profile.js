ad1156ddca61510de1f3388da8466fa5805fa228
Andrez18 <ieca.andresgomez@gmail.com>
2025-07-08 17:53:34 -0500

    Actualizar la visualización de perfil para usar propiedades de usuario consistentes

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -70,12 +70,12 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(usuario)
 
     profileInfo.innerHTML = `
-      <p><strong>Nombre:</strong> ${usuario.nombre || 'Sin nombre'}</p>
-      <p><strong>Correo:</strong> ${usuario.correo || 'Sin correo'}</p>
-      <p><strong>Fecha de registro:</strong> ${usuario.fecha_registro 
+        <p><strong>Nombre:</strong> ${usuario.name || usuario.nombre ||'Sin nombre'}</p>
+        <p><strong>Correo:</strong> ${usuario.email || usuario.correo ||'Sin correo'}</p>
+              <p><strong>Fecha de registro:</strong> ${usuario.fecha_registro 
         ? new Date(usuario.fecha_registro).toLocaleDateString()
         : 'No disponible'}</p>
-      <p><strong>Nivel actual:</strong> ${usuario.nivel || 'No asignado'}</p>
-      <p><strong>Puntos:</strong> ${usuario.puntos || 0}</p>
+        <p><strong>Nivel actual:</strong> ${usuario.nivel || 'No asignado'}</p>
+        <p><strong>Puntos:</strong> ${usuario.puntos || 0}</p>
     `;
 });
